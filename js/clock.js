ed2a909f838388bce0692b8e040ca0f50028870a
Ronin Koi <lightningblade.901@gmail.com>
2024-05-08 21:34:21 -0400

    added comments to clock script. last commit of the day

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,11 @@
-console.log('hello')
-
 // clock functions
+
+// creates new time function
+// for future development: create time and set to vars so user can change display as they like
 function clockExecution() {
   const d = new Date();
   document.getElementById('clockDisplay').innerHTML = d.toLocaleTimeString();
 }
 
+// setInterval automatically update clock
 setInterval(clockExecution, 1000);

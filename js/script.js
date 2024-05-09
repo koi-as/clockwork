a66507e07185151491cd4c997928cbfb4e54648c
Ronin Koi <lightningblade.901@gmail.com>
2024-05-08 21:06:47 -0400

    added clock function to display clock

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('hi');
+
+// clock functions
+function clockExecution() {
+  const d = new Date();
+  document.getElementById('clockDisplay').innerHTML = d.toLocaleTimeString();
+}
+
+// timer functions
